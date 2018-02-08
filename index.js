dcf7f6e50a6249e0d5501b839fbe235c7f5e21ad
SriRamLakshmiNarasimhan <sriramnarasimhan1990@gmail.com>
2018-02-08 11:40:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -21,7 +21,10 @@
             this.$textarea.on('keyup', this.addMessageEnter.bind(this));
         },
               luisCall: function (value) {
+				  //var decrypted = CryptoJS.AES.decrypt("U2FsdGVkX19Glask4Gxi9ajZUYLuGmcnz/1RW/mL3jV9V66raqG6U/s3ZiCHg/96GzMinXRj5G8cHXzF18blIw==", "Sre");
 				   var params = {
+            // These are optional request parameters. They are set to their default values.
+			//"subscription-key":"5bd9c482dba94e4a8eb8a595befcaff9",
 			"subscription-key":CryptoJS.AES.decrypt("U2FsdGVkX19Glask4Gxi9ajZUYLuGmcnz/1RW/mL3jV9V66raqG6U/s3ZiCHg/96GzMinXRj5G8cHXzF18blIw==", "Sre").toString(CryptoJS.enc.Utf8),
 			"verbose": "true",
             "timezoneOffset": "0",
@@ -29,7 +32,11 @@
         };
             var _this = this;
 			 $.ajax({
-            url: "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/340cf197-e7e6-476f-a4d1-8d2939eb7d04?" + $.param(params),
+            url: "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/88cfdca1-3702-49fe-9aa6-210e6828529e?" + $.param(params),
+/*             beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","key=");
+            }, */
             type: "GET",
             data: "{body}",
         })
@@ -45,7 +52,7 @@
 			}.bind(_this), 1500); 
         })
         .fail(function() {
-           alert("Technical error: Check internet connection or retry after sometime");
+           alert("Technical error: Check internet connection or notify at sriramnarasimhan1990@gmail.com");
         })
 			  },
         render: function () {
@@ -67,6 +74,7 @@
             this.render();
         },
         addMessageEnter: function (event) {
+            //Enter key event
             if (event.keyCode === 13) {
                 this.addMessage();
             }
@@ -87,78 +95,28 @@
 		chatResponse: function(jsonData){
 			var outputMessage;
 			switch(jsonData.topScoringIntent.intent){
-				case "greetings":
-					outputMessage = "Hey, I am CricBot! You can ask me for cricket news. Wanna know more? Ask more!";
+				case "Generic Greetings":
+					outputMessage = "Hey! What's your good name?";
 					break;
-				case "player_info_games_played":
-					if(jsonData.entities.length > 0){
-						var name = this.capitalizeFirstLetter(jsonData.entities[0].resolution.values[0]);
-							switch(name){
-								case "MS Dhoni":
-									outputMessage = name + " has played 90 test, 312 ODI and 131 FC matches"
-									break;
-								case "Sachin Tendulkar":
-									outputMessage = name + " has played 200 test, 463 ODI and 310 FC matches"
-									break;
-								case "Shane Watson":
-									outputMessage = name + " has played 59 test 190 ODI, 58 T20 International matches"
-									break;
-								case "Steve Smith":
-									outputMessage = name + " has played 61 test, 103 ODI and 110 FC matches"
-									break;
-								default:
-									outputMessage = "It is a yorker to ask on "+name+"! I am trained to answer only about MS Dhoni, Sachin Tendulkar, Shane Watson and Steve Smith."
-									break;
-							}
-						break;
-					}
-					else{
-						outputMessage = "Can you be more specific? You can ask for the number of matches Steven Smith have played so far";
-						break;
-					}
+				case "Generic Good Answer":
+					outputMessage = "That's great!\nYou can ask for my intro to know more..";
 					break;
-				case "player_info_sixes":
-					outputMessage = "Question on number of sixes. I don't have data yet.";
+				case "Generic Bad Answer":
+					outputMessage = "Hmm..\nMay be you can ask for my intro, if you're interested.";
 					break;
-				case "player_info_fours":
-					outputMessage = "Question on number of fours. I don't have data yet.";
+				case "Generic Good Wishes":
+					outputMessage = "Cool!";
 					break;
-				case "player_info_duck":
-					outputMessage = "Question on number of duck out. I don't have data yet.";
+				case "Generic Bad Wishes":
+					outputMessage = "I'm sorry!";
 					break;
-				case "stadium_info":
-					if(jsonData.entities.length > 0){
-						var type = jsonData.entities[0].resolution.values[0];
-						switch(type){
-								case "biggest":
-									outputMessage = "With a capacity of 100,024, Melbourne Cricket Ground is the biggest in the world."
-									break;
-								case "highest":
-									outputMessage = "At an altitude of 2144m, Dharamsala, (Chail, India) is the highest cricket ground in the world."
-									break;
-								case "smallest":
-									outputMessage = "The Carisbrook (Dunedin, New Zealand) is regarded as one of the smallest grounds in the world."
-									break;
-								default:
-									outputMessage = "Can you please rephrase? You can ask for the biggest cricket ground in the world"
-									break;
-							}
-					}
-					else{
-						outputMessage = "Can you be more specific? You can ask for the biggest cricket ground in the world";
-						break;
-					}
+				case "Generic Good Bye":
+					outputMessage = "Good Bye!\nHave a nice day :)";
 					break;
-				case "other_info_cricket_news":
-					outputMessage = "";
+				case "Generic Unscoped":
+					outputMessage = "Hush hush! I am not going to answer this.";
 					break;
-				case "purchase_tickets":
-					outputMessage = "Watching match in live is a great experience in itself! Buy tickets at https://www.cricket.com.au/tickets?state=all&type=All";
-					break;
-				case "purchase_accessories":
-					outputMessage = "New items are available for sale. Check out at https://shop.cricket.com.au/";
-					break;
-				case "something":
+				case "Introduction Self":
 					if(jsonData.entities.length > 0){
 						var name = this.capitalizeFirstLetter(jsonData.entities[0].entity);
 						outputMessage = "Hey "+ name + "!\nHow're you doing?";
@@ -168,6 +126,54 @@
 						outputMessage = "Hmm..\nHow're you doing?";
 						break;
 					}
+				case "Introduction Question":
+					outputMessage = "This is Sri Ram, a post-graduate in Applied Mathematics.\nWorking as Sr. Software Engineer in Accenture Solutions Pvt Ltd, I'm passionate about cycling, writing Tamil peoms and learning to write awesome code.\nYou can ask specific questions to know more.";
+					break;
+				case "Education General":
+					outputMessage = "I did my schooling was from Sri Vidya Mandir (Salem, IN) and chose Mathematics for college studies.\nTo know more, you can ask specific questions on school or college education or extra curricular activities.";
+					break;
+				case "Education School":
+					outputMessage = "With interests in Science and Mathematics, I passed my class 12 from Sri Vidya Mandir (Salem, IN) in the year 2008 and class 10 in the year 2006.";
+					break;
+				case "Education College":
+					outputMessage = "From Agurchand Manmull Jain College (affiliated to Madras University, Chennai, IN), with Statistics and Mechanics as allied subjects, I completed Bachelor of Science in Mathematics in the year 2011.\nWith interest in Analysis and Abstract Algebra and inspired by Prof. Nandagopal, I studied Master of Science in Applied Mathematics from Madras Institute of Technology (Anna University, Chennai, IN).";
+					break;
+				case "Education Other Activities":
+					outputMessage = "Apart from regular studies I learnt Karate (brown belt - 2nd kyu), Hindi, pagged 1st prizes in Bhagavath Gita chanting and an active Scout member (Rajyapuraskar scout).\nI'm also a department topper during college.\nAlbum for the same is available at https://photos.app.goo.gl/1ZCND12rkoFoX2jA3.";
+					break;
+				case "Education Unscoped":
+					outputMessage = "Can you please rephrase? You can ask about school or college education for more details.";
+					break;
+				case "Hobby General":
+					outputMessage = "I am passionate about cycling in my Hero Octane and sometimes swim too and write peoms in Tamil too.\nI also focus on building awesome programs and learning and trying to make wonders with Arduino UNO that helps people in someway.\nSome of my peoms and baby steps in Arduino UNO can be seen here https://photos.app.goo.gl/aCAULA1vzwpHuW1A2";
+					break;
+				case "Hobby Unscoped":
+					outputMessage = "Can you please rephrase? You can ask about hobby for more details.";
+					break;
+				case "Career General":
+					outputMessage = "I strongly believe that Mathematics is not just about blackboard and chalk piece and I chose to join Accenture in August 2013 right after my post-graduation.\nMy initial role was in testing where I verified BI services and CRMs.\nI moved towards automation testing using Selenium both with Java and C# and I'm slowly stepping into AI/ML.\nYou can ask specific questions on skillset or awards or certifications to know more.";
+					break;
+				case "Career Awards":
+					outputMessage = "I received Accenture Stellar award during Q4 of FY14 for compatibility testing and for driving scrum.\nI also received Accenture APEX award for driving Remote Desktop to Citrix VDESK transition for entire team and I'm awarded Accenture Celebrates Excellence (ACE) twice for performance testing and for codeless automation solution.";
+					break;
+				case "Career Office":
+					outputMessage = "I'm working  for Accenture Solutions Pvt Ltd (Chennai, IN) since August 2013.";
+					break;
+				case "Career Skillset":
+					outputMessage = "My skillset includes - BI testing, AMDOCS & Salesforce CRM testing, Selenium with Java & C#, programming in C# - WinForms, WPF, MVC, WebAPI solutions, setting up and handling Microsoft & Google AI services and CMU Sphinx. I am also equipping with Python.";
+					break;
+				case "Career Certifications":
+					outputMessage = "Having started my career as tester, I'm ISTQB certified Foundation and Agile tester and also Vskill's Certified Selenium Professional.\nI'll soon be Microsoft Technology Associate in Python";
+					break;
+				case "Career Change":
+					outputMessage = "Forseeing future with neural networks and artificial intelligence, I'm open to taking challenges if it is for serving a cause.\nAnd 10 years down the line, I want to see myself as a building block for the cause";
+					break;
+				case "Career Unscoped":
+					outputMessage = "Can you please rephrase? You can ask about career for more details.";
+					break;
+				case "Chatbot":
+					outputMessage = "Written in JavaScript, the chatbot is powered by Microsoft Language Understanding Intelligent Services.\nCode can be modified as needed and can be cloned from https://github.com/SriRamLakshmiNarasimhan/chatbot. \nFor more details on Microsoft LUIS, please check https://www.luis.ai/home.";
+					break;
 				default:
 					if(jsonData.topScoringIntent.intent == "None"){
 						outputMessage = "I'm afraid if I understood the context. You can ask for my introduction or about this chatbot.";
